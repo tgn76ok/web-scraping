bdb742a70a45e723cc6826a1c6644c292a9ecda5
Tigaz <62727856+tgn76ok@users.noreply.github.com>
2023-12-15 11:58:00 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -3,38 +3,53 @@ from time import sleep,time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
+
+from selenium.webdriver.support import expected_conditions as EC
+from threading import Thread
+def votar():
+    chrome_options = webdriver.ChromeOptions()
+    # proxy = '200.155.180.166:8080'
+    chrome_options.add_argument("--disable-images")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-cache")
+    # chrome_options.add_argument(f"--proxy-server={proxy}")
+    chrome_options.page_load_strategy = 'eager'
+
+    navegador = webdriver.Chrome(options=chrome_options,service=Service(ChromeDriverManager().install()))
+
+    start_time = time()
+    navegador.get("https://www.polemicaparaiba.com.br/politica/os-melhores-de-2023-na-sua-opiniao-qual-foi-o-melhor-e-mais-atuante-deputado-federal-da-paraiba-neste-ano-vote/")
+    # WebDriverWait(navegador, 25).until(EC.element_to_be_clickable((By.ID,"choice-713b8dc9-0de5-4623-9005-f5971115fd4c-selector")))
+    # WebDriverWait(navegador, 25).until(EC.element_to_be_clickable((By.XPATH,"/div[4]/button")))
+
+    # Aguarda até que o elemento esteja presente na página
+    try:
+        js_code = "document.getElementById('choice-713b8dc9-0de5-4623-9005-f5971115fd4c-selector').click();"
+        navegador.execute_script(js_code)
+        js_code1 = "document.querySelector('button.totalpoll-button.totalpoll-button-primary.totalpoll-buttons-vote').click();"
+        navegador.execute_script(js_code1)
+
+        print(f'--------------------------')
+        print(f'Processor deu bom')
+        print(f'--------------------------')
+        print(f"Tempo de execução: {time() - start_time} segundos")
+        # Fecha o navegador ao finalizar
+    except WebDriverException as e:
+        print("erro")
+        print(f'Processor deu ruim')
+        new = str(e).split('Stacktrace')[0]
+        new = str(e).split('\n')[0]
+        print(f"Tempo de execução: {time() - start_time} segundos")
+        print(new)
+        sleep(100)
+    navegador.quit()
 
 
-start_time = time()
-chrome_options = webdriver.ChromeOptions()
-
-# chrome_options.add_argument("--disable-images")
-# chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-cache")
-chrome_options.page_load_strategy = 'eager'
-
-navegador = webdriver.Chrome(options=chrome_options)
-
-navegador.get("sua urls")
-
-# WebDriverWait(navegador, 25).until(EC.element_to_be_clickable((By.ID,"choice-713b8dc9-0de5-4623-9005-f5971115fd4c-selector")))
-# WebDriverWait(navegador, 25).until(EC.element_to_be_clickable((By.XPATH,"/div[4]/button")))
 
 
-
-# Aguarda até que o elemento esteja presente na página
-js_code = "document.getElementById('choice-713b8dc9-0de5-4623-9005-f5971115fd4c-selector').click();"
-navegador.execute_script(js_code)
-print('olamniudoasdfdasgf')
-js_code1 = "document.querySelector('button.totalpoll-button.totalpoll-button-primary.totalpoll-buttons-vote').click();"
-navegador.execute_script(js_code1)
-
-print(f"Tempo de execução: {time() - start_time} segundos")
-# Fecha o navegador ao finalizar
-sleep(10)
-navegador.quit()
-
-
-
-
+while True:
+    votar()
